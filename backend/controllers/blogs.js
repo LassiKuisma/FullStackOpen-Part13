53463182279e9849d8a23a204f186bf6d39dc5c0
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-08-02 18:56:04 +0300

    example: include user, exclude id's

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -22,7 +22,13 @@ const tokenExtractor = (req, res, next) => {
 };
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['uderId'] },
+    include: {
+      model: User,
+      attributes: ['name'],
+    },
+  });
   res.json(blogs);
 });
 
