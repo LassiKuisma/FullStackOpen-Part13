00996df1b80487320ae2fe8fd4705f66a0a71662
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-08-03 16:37:53 +0300

    exercise 13.16

---
+++ backend/controllers/authors.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+const { fn, col } = require('sequelize');
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll({
+    attributes: [
+      'author',
+      [fn('COUNT', col('author')), 'articles'],
+      [fn('SUM', col('likes')), 'likes'],
+    ],
+    group: 'author',
+    order: [['likes', 'DESC']],
+  });
+
+  res.json(blogs);
+});
+
+module.exports = router;
