bd6288323dcacbd08c834a2e9aa78e7c108bf55c
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-08-10 18:55:36 +0300

    exercise 13.22

--- backend/controllers/reading_lists.js
+++ backend/controllers/reading_lists.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const { Blog, User, ReadingList } = require('../models');
 
+const { tokenExtractor } = require('../util/tokenExtractor');
+
 router.post('/', async (req, res) => {
   const userId = req.body.userId;
   const blogId = req.body.blogId;
@@ -25,6 +27,41 @@ router.post('/', async (req, res) => {
   });
 
   res.json(listItem);
+});
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const markedAsRead = req.body.read;
+
+  if (markedAsRead === undefined) {
+    return res.status(400).send({
+      error: "Request didn't specify whether to mark blog as read or unread",
+    });
+  }
+
+  const blogId = req.params.id;
+
+  const blog = await Blog.findByPk(blogId);
+  if (!blog) {
+    return res.status(404).send({ error: 'Blog not found' });
+  }
+
+  const user = await User.findByPk(req.decodedToken.id);
+
+  if (!user) {
+    return res.status(404).send({ error: 'User not found' });
+  }
+
+  const entry = await ReadingList.findOne({
+    where: {
+      userId: user.id,
+      blogId: blog.id,
+    },
+  });
+
+  entry.read = markedAsRead;
+  await entry.save();
+
+  res.json(entry);
 });
 
 module.exports = router;
