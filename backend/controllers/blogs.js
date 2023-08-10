422445ced43ea6c832bc94c9dae50be220729444
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-08-10 18:32:36 +0300

    move token extractor to util

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,26 +1,12 @@
-const jwt = require('jsonwebtoken');
-const { SECRET } = require('../util/config');
 const router = require('express').Router();
 
 const { Blog, User } = require('../models');
 const { Op } = require('sequelize');
 
+const { tokenExtractor } = require('../util/tokenExtractor');
+
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id);
-  next();
-};
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: 'token invalid' });
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' });
-  }
   next();
 };
 
